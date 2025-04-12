7243cfd9e6baa5328ceb966bb7c39b5877f32b7f
arctair <arctair>
2025-04-11 21:00:39 -0700

    podman

--- build-and-run.zsh
+++ build-and-run.zsh
@@ -4,11 +4,11 @@ seed="$1"
 
 set -e
 
-docker build -t wavy-bacon .
-id=$(docker run -dip 25565:25565 wavy-bacon)
+podman build -t wavy-bacon .
+id=$(podman run -dip 25565:25565 wavy-bacon)
 echo $id
 if [ "$seed" != "" ]; then
-  docker cp $seed $id:/home/minecraft/backups/
-  docker exec $id tar xf /home/minecraft/backups/$seed
+  podman cp $seed $id:/home/minecraft/backups/
+  podman exec $id tar xf /home/minecraft/backups/$seed
 fi
-docker exec -d $id /bin/start
+podman exec -d $id start
